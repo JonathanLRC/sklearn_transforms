d4cd425f4847fe3a99013b67f3072fc9e302bacf
Jonathan Ramirez <jonathan.ramirezc08@anahuac.mx>
2020-12-05 11:04:57 -0600

    Revert "Update setup.py"
    This reverts commit d0d8aacb37bdade5086225c64841571dfccbf4a3.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
       name='my_custom_sklearn_transforms',
-      version='2.0',
+      version='1.0',
       description='''
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
